40f46838a6c81ff4a8085e04756e9b4aba3de388
Xiaoning He <xh1019@ic.ac.uk>
2021-01-09 12:59:28 +0000

    Titanic cross validation

--- titanic/solution.py
+++ titanic/solution.py
@@ -11,22 +11,20 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_absolute_error
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
 train_data = pd.read_csv("./data/train.csv", index_col='PassengerId')
 test_data = pd.read_csv("./data/test.csv", index_col='PassengerId')
 
-print(train_data.head())
+X_train = train_data
+y_train = train_data.Survived
 
-y = train_data.Survived
-X_train, X_valid, y_train, y_valid = train_test_split(train_data, y, train_size=0.8, test_size=0.2, random_state=42)
-
-print(X_train.isna().sum())
-
-num_cols = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+num_cols = ['Pclass', 'SibSp', 'Parch', 'Fare']
 num_transformer = SimpleImputer(strategy='constant')
+
+avg_cols = ['Age']
+avg_transformer = SimpleImputer(strategy='median')
 
 cat_cols = ['Sex', 'Embarked']
 cat_transformer = Pipeline(steps=[
@@ -34,26 +32,45 @@ cat_transformer = Pipeline(steps=[
   ('onehot', OneHotEncoder(handle_unknown='ignore')),
 ])
 
-cols = cat_cols + num_cols
+cols = cat_cols + num_cols + avg_cols
 X_train = X_train[cols].copy()
-X_valid = X_valid[cols].copy()
 X_test = test_data[cols].copy()
 
 preprocessor = ColumnTransformer(transformers=[
     ('cat', cat_transformer, cat_cols),
     ('num', num_transformer, num_cols),
+    ('avg', avg_transformer, avg_cols),
   ])
-model = RandomForestClassifier(n_estimators=100, random_state=42)
 
+def get_score(n_estimators, preprocessor, X_train, y_train):
+  model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+  pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', model),
+  ])
+  scores = -1 * cross_val_score(pipeline, X_train, y_train,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+  return scores.mean()
+
+ns = [30, 60, 100, 150, 300, 500]
+results = {n: get_score(n, preprocessor, X_train, y_train) for n in range(103, 112, 1)}
+
+# import matplotlib.pyplot as plt
+
+# plt.plot(list(results.keys()), list(results.values()))
+# plt.show()
+
+best_value = min(results, key=results.get)
+print(best_value)
+
+model = RandomForestClassifier(n_estimators=best_value, random_state=42)
 pipeline = Pipeline(steps=[
   ('preprocessor', preprocessor),
   ('model', model),
 ])
 
 pipeline.fit(X_train, y_train)
-preds = pipeline.predict(X_valid)
-
-print('MAE:', mean_absolute_error(y_valid, preds))
 
 preds_test = pipeline.predict(X_test)
 output = pd.DataFrame({
